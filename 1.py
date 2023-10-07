f67ae8884be4192abd7d44007ab626c25e37030c
Kenzo <baha03355@mail.ru>
2023-10-07 19:59:59 +0700

    'Pofig'

--- 1.py
+++ 1.py
@@ -1,19 +1,34 @@
-﻿python
-import random
+﻿import random
 import time
 import matplotlib.pyplot as plt
 
-sizes = range(1000, 100001, 1000)  
+# тупо списки
+sizes = []
 times = []
 
-for size in sizes:
-    start_time = time.time()  
-    random_list = [random.randint(0, 100) for _ in range(size)]  
-    end_time = time.time()  
-    times.append(end_time - start_time)  
+# от 1тыс. до 100тыс. с шагом 1тыс.
+for size in range(1000, 101000, 1000):
+# стартуем
+start = time.time()
 
-plt.plot(sizes, times)  
-plt.xlabel('Размер списка')
-plt.ylabel('Время создания (сек)')
+# делаем список
+r = [random.randint(1, 100) for _ in range(size)]
+
+# конченое время
+end = time.time()
+c = end - start
+
+# размер и время
+sizes.append(size)
+times.append(c)
+
+# график
+plt.figure(figsize=(10, 6))
+plt.plot(sizes, times, marker='o', linestyle='-')
 plt.title('Зависимость времени создания списка от его размера')
+plt.xlabel('Размер списка')
+plt.ylabel('Время создания (секунды)')
+plt.grid(True)
+
+# показываем
 plt.show()
